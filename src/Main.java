1be4dbeb65e31d0d62507e58e1f0ccf5cd45a8c2
Karina Reyes <reyesaguilark@gmail.com>
2026-06-03 20:58:11 -0600

    se agrego un comentario donde se inicia un nuevo modulo

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola, Mundo!");
         //pruebas para ultimo reto
+         System.out.println("aqui se ejecutara otro modulo");
     }
 }
